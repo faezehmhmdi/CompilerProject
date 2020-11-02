ad8d46ca919d76477ddcd9145e9dd38c7296c21a
Faezeh Mohammadi <59282324+faezehmhmdi@users.noreply.github.com>
2020-11-03 01:04:27 +0330

    Writing the LexTokens in File

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ lexer = lex.build()
 file = open("test3.txt")
 text_input = file.read()
 file.close()
+result = open("result3.txt", "w")
 lexer.input(text_input)
 while True:
     tok = lexer.token()
     if not tok:
         break
-    print(tok)
+    result.write(str(tok))
+    result.write("\n")
+result.close()
