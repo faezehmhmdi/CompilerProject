d4cb8ed139c6719bff98deeb362df54376eb5df4
Faezeh Mohammadi <59282324+faezehmhmdi@users.noreply.github.com>
2020-12-03 19:53:50 +0330

    building parser added.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-from lexer import Lexer
+from classes.lexer import Lexer
+from classes.parser import Parser
 
 lex = Lexer()
 lexer = lex.build()
-file = open("test3.txt")
+file = open("./tests/ph2test2.txt")
 text_input = file.read()
 file.close()
-result = open("result3.txt", "w")
+# result = open("result3.txt", "w")
 lexer.input(text_input)
-while True:
-    tok = lexer.token()
-    if not tok:
-        break
-    result.write(str(tok))
-    result.write("\n")
-result.close()
+# while True:
+#     tok = lexer.token()
+#     if not tok:
+#         break
+#     result.write(str(tok))
+#     result.write("\n")
+# result.close()
+parser = Parser()
+parser.build().parse(text_input, lexer, False)
